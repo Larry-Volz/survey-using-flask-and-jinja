7330c3df8669743b9aec351612fd4b1dd94b04d8
Larry Volz <imaginologist@gmail.com>
2021-01-27 20:46:31 -0500

    working on referenced before assignment issue

--- app.py
+++ app.py
@@ -8,28 +8,21 @@ app.config['SECRET_KEY'] = 'FOO'
 app.debug=True
 debug=DebugToolbarExtension(app)
 
-
-
+responses = []
+q_number = 0
+title = satisfaction_survey.title
+instructions = satisfaction_survey.instructions
+questions = [question for question in satisfaction_survey.questions]
+q_idx = 0
 
 
 @app.route('/')
 def survey_start_page():
-    global responses
-    global q_number
-    global title
-    global instructions
-    global questions
-    global q_idx
-    responses = []
-    q_number = 0
-    title = satisfaction_survey.title
-    instructions = satisfaction_survey.instructions
-    questions = [question for question in satisfaction_survey.questions]
-    q_idx=0
+    
     return render_template('index.html', survey_title=title, survey_instructions=instructions)
 
 @app.route('/questions/<int:q_number>', methods=["GET","POST"])
-def questions_page(q_number, questions):
+def questions_page(q_number):
     
     q_num=request.form["q_number"]
     num_q=len(questions)
