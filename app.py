b687e7b92936455326bfee288129b6a0281770d0
Larry Volz <imaginologist@gmail.com>
2021-01-27 20:09:48 -0500

    missing positional argument questions

--- app.py
+++ app.py
@@ -8,19 +8,34 @@ app.config['SECRET_KEY'] = 'FOO'
 app.debug=True
 debug=DebugToolbarExtension(app)
 
-responses = []
+
+
 
 
 @app.route('/')
 def survey_start_page():
+    global responses
+    global q_number
     global title
     global instructions
+    global questions
+    global q_idx
+    responses = []
+    q_number = 0
     title = satisfaction_survey.title
     instructions = satisfaction_survey.instructions
+    questions = [question for question in satisfaction_survey.questions]
+    q_idx=0
     return render_template('index.html', survey_title=title, survey_instructions=instructions)
 
 @app.route('/questions/<int:q_number>', methods=["GET","POST"])
-def questions_page():
-    q_num = POSTS.get(q_number, "Post Not Found")
-    return render_template('questions.html')
+def questions_page(q_number, questions):
+    
+    q_num=request.form["q_number"]
+    num_q=len(questions)
+    if q_idx < num_q:
+        q_idx += 1
+        return render_template('questions.html',num_q=q_idx)
+    else:
+        return render_template ('thank_you.html')
 
